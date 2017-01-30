b134278c30f4760ee03255dd0fe7209ac4f8b75e
Christopher Duhan <chris.m.duhan@gmail.com>
2017-01-29 18:05:45 -0600

    To turn in

--- Assignments/homework-01.py
+++ Assignments/homework-01.py
@@ -5,6 +5,8 @@ Assignment: Homework 1 - Lists and Dictionaries
 Due: 31 Jan @ 11:00 a.m.
 """
 
+#A:
+
 a = [1, 5, 4, 2, 3] 
 print(a[0], a[-1])
 # Prints: 1 3
@@ -24,23 +26,30 @@ print(a)
 # Prints: [1, [5, 1], 4, 2, 6]
 print()
 
+#B:
+
 x = [3, 1, 2, 1, 5, 1, 1, 7]
 def remove_all(el,lst):
     for i in lst:
-      lst.remove(el)
-      
+        lst.remove(el)
+        
 remove_all(1,x)
 print(x)
 print()
 
+#C:
+
 lst = [1, 2, 4, 2, 1]
 def add_this_many(x, y, lst):
     for i in lst:
-      if i == x:
+        if i == x:
         lst.append(y)
+        
 add_this_many(1, 5, lst)
 print(lst)
 print()
+
+#D:
 
 a = [3, 1, 4, 2, 5, 3]
 print(a[:4])
@@ -65,24 +74,31 @@ print(a[::-1])
 # Prints: [3, 5, 2, 4, 1, 3]
 print()
 
+#E:
+
 x = [3, 2, 4, 5, 1]
 def reverse(lst):
     for i in range(round(len(lst)/2)):
-      swap = lst[i]
-      lst[i] = lst[len(lst)-(i + 1)]
-      lst[len(lst)-(i + 1)] = swap
-      
+        #Using round to get an interger returned
+        swap = lst[i]
+        lst[i] = lst[len(lst)-(i + 1)]
+        lst[len(lst)-(i + 1)] = swap
+        
 reverse(x)
 print(x)
 print()
 
-x = [1, 2, 3, 4, 5]
+#F:
+
 def rotate(lst, k):
-  return lst[(k-1):] + lst[:(k-1)]
+    #Using concatenation
+    return lst[(k-1):] + lst[:(k-1)]
   
 xrot = rotate(x, 3)
 print(xrot)
 print()
+
+#H:
 
 superbowls = {'joe montana': 4, 'tom brady':3, 'joe flacco': 0}
 superbowls['peyton manning'] = 1
@@ -111,27 +127,35 @@ print(superbowls)
 
 superbowls[('steelers', '49ers')] = 11
 print(superbowls)
-#Prints: {3: 'cat', ('steelers', '49ers'): 11, 'peyton manning': 1, ('eli manning', 'giants'): 5, 'tom brady': 3, 'joe flacco': 1, 'joe montana': 4}
+"""Prints: {3: 'cat', ('steelers', '49ers'): 11, 'peyton manning': 1, ('eli manning', 'giants'): 5,
+'tom brady': 3, 'joe flacco': 1, 'joe montana': 4}
+"""
 print()
+
+#I:
 
 d = {1:{2:3, 3:4}, 2:{4:4, 5:3}} 
 def replace_all(d, x, y):
     for k in d.keys(): 
-      if type(d[k]) == dict: 
-        replace_all(d[k], x, y)
-      elif d[k] == x: 
-        d[k] = y 
+        if type(d[k]) == dict: 
+            replace_all(d[k], x, y)
+        elif d[k] == x: 
+            d[k] = y
+            
 replace_all(d,3,1)
 print(d)
 print()
 
+#J:
+
 d = {1:2, 2:3, 3:2, 4:3}
 def rm(d, x):
+    #Using a list of keys to remove the key/value pairs
     delete = []
     for k in d.keys():
-      if d[k] == x:
-        delete.append(k)
+        if d[k] == x:
+            delete.append(k)
     for k in delete:
-      del d[k]
+        del d[k]
 rm(d,2)
 print(d)
